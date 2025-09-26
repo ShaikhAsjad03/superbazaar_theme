4fcf964138d1fbe429757b3a1f2c83a1f77cd38a
Shaikh Asjad <shaikhasjad29@gmail.com>
2025-09-26 16:38:29 +0530

    changes

--- src/theme/theme1/Home/components/Banner.jsx
+++ src/theme/theme1/Home/components/Banner.jsx
@@ -45,6 +45,40 @@ const Banner = ({ bannerdata }) => {
         ))}
 
       </Swiper>
+      <style jsx global>{`
+         .swiper-pagination {
+           bottom: 15px !important;
+           display: flex;
+           justify-content: center;
+           gap: 12px;
+         }
+         .swiper-pagination-bullet {
+           position: relative;
+           width: 16px;
+           height: 16px;
+           background: transparent;
+           border-radius: 50%;
+           border: 2px solid #fff;
+           opacity: 1;
+           transition: all 0.3s;
+           box-sizing: border-box;
+         }
+         .swiper-pagination-bullet::after {
+           content: "";
+           position: absolute;
+           top: 50%;
+           left: 50%;
+           transform: translate(-50%, -50%);
+           width: 5px;
+           height: 5px;
+           background: white;
+           border-radius: 50%;
+         }
+         .swiper-pagination-bullet-active::after {
+           width: 9px;
+          height: 9px;
+         }
+       `}</style>
     </section>
   );
 };
